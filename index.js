5deeb2d1998d4f0d36b77a6e5247c5f737e63099
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-03 19:33:37 -0300

    vs-1.0.3

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ function last() {
 
 String.prototype.last = last
 Array.prototype.last = last
-
-//            0         1        2      3  ....
-//            0                         -3       -2       -1
-let lista = ['Python', 'Kotlin', 'C#', 'Rust', 'Js', 'Java']
-let nome = 'Gustavo'
-
-console.log(lista.last())
-console.log(nome.last())
